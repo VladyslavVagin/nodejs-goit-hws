0ff211fa4042a75066f0751de5e0081c0752b509
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-01-11 10:34:41 +0100

    update
    .env

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+// @ts-nocheck
 const app = require("./app");
 const mongoose = require("mongoose");
-const {DB_HOST} = require("./config");
+const {DB_HOST} = process.env;
 
 mongoose
   .connect(DB_HOST)
