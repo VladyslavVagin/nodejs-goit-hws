e80b49bf2a229d3b3e4895daa7c309fd20708da5
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-01-14 13:09:30 +0100

    update

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -7,13 +7,14 @@ const getAll = async (req, res) => {
   const { page = 1, limit = 10, favorite } = req.query;
   const skip = (page - 1) * limit;
   const query = favorite !== undefined ? {owner, favorite} : {owner};
-  const result = await Contact.find(query, "-owner", { skip, limit }); // .populate("owner", "name email"); для розширеної відповіді з інформацією про user
+  const result = await Contact.find(query, "owner name phone email favorite", { skip, limit }); // .populate("owner", "name email"); для розширеної відповіді з інформацією про user
   res.json(result);
 };
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await Contact.findById(contactId);
+  const { _id } = req.user;
+  const result = await Contact.findOne({_id: contactId, owner: _id});
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -28,7 +29,8 @@ const add = async (req, res) => {
 
 const deleteContact = async (req, res) => {
   const { contactId } = req.params;
-  const result = await Contact.findByIdAndDelete(contactId);
+  const { _id } = req.user;
+  const result = await Contact.findOneAndDelete({_id: contactId, owner: _id});
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -39,7 +41,8 @@ const deleteContact = async (req, res) => {
 
 const update = async (req, res) => {
   const { contactId } = req.params;
-  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+  const { _id } = req.user;
+  const result = await Contact.findOneAndUpdate({_id: contactId, owner: _id}, req.body, {
     new: true,
   });
   if (!result) {
@@ -50,7 +53,8 @@ const update = async (req, res) => {
 
 const updateFavorite = async (req, res) => {
   const { contactId } = req.params;
-  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+  const { _id } = req.user;
+  const result = await Contact.findOneAndUpdate({_id: contactId, owner: _id}, req.body, {
     new: true,
   });
   if (!result) {
