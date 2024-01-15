bb259afddcf8ad83b761f569b10ab6461a39238b
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-01-15 12:51:01 +0100

    update
    Done - all works without tests

--- models/user.js
+++ models/user.js
@@ -37,6 +37,10 @@ const userSchema = new Schema(
       type: String,
       default: "",
     },
+    avatarURL: {
+      type: String,
+      required: true,
+    }
   },
   { versionKey: false, timestamps: true }
 );
